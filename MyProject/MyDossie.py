f6d3c8e80795b81a9a9726114e939da424a471de
Millyzinha11 <kamilli.alessandra11@gmail.com>
2024-07-12 09:53:48 -0300

    Add files via upload

---
+++ MyProject/MyDossie.py
@@ -0,0 +1,22 @@
+#Inserção de Dados...
+print("Digite o seu nome:")
+MyName = str(input())
+print("Digite a sua idade")
+MyAge = int(input())
+print("Digite a sua altura:")
+MyHeigh= float(input())
+
+# Declaração de variáveis
+MyMaritalStatus= "Namorando"
+Weight= 63,0
+Myfavoritecolor= "Lavander"
+MyFavoriteFood= "Strogonoff"
+
+#Exibição do conteúdo das variáveis
+print(f"Meu nome é {MyName},.")
+print(f"Minha altura é de {MyHeigh} metros")
+print(f"Minha idade é {MyAge} anos.")
+print(f"Meu estado civil é {MyMaritalStatus}")
+print(f"Meu peso é {Weight} metros")
+print(f"Minha cor favorita é {Myfavoritecolor}")
+print(f"Minha comida favorita é {MyFavoriteFood}")
