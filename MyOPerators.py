7d33abf3430456c5d0ebb5ef85b7fac9588a591e
Kamilly Alessandra Oliveira da Silva <kamilli.alessandra11@gmail.com>
2024-07-12 11:40:36 -0300

    Aprendendo expressões complexas e aplicando.

---
+++ MyOPerators.py
@@ -0,0 +1,16 @@
+# Demonstração de expressões complexas
+
+# Declaração de variáveis que iremos usar...
+X = 10; Y = 20; Z = 30
+
+valor = X * (Y / 10 + Z / 10) - 25
+print("O resultado da expressão #1 e:", valor)
+
+valor = ((X - Y) + 10 + (Z - X ) ) / 3
+print("O resultado da expressão #2 e:", valor)
+
+valor = (X + (Y * 2 + (Z / 10 ))) - 18
+print("O resultado da expressão #3 e:", valor)
+
+# Antes de executar estes códigos, seria
+# possível calcular "de cabeça" estes valores?
