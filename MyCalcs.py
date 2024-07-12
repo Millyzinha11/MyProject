91c2a9cbd58cc63e42d262996c9fe9b564309d32
Kamilly Alessandra Oliveira da Silva <kamilli.alessandra11@gmail.com>
2024-07-12 11:30:35 -0300

    Aprendendo a fazer calculos em programação

---
+++ MyCalcs.py
@@ -0,0 +1,15 @@
+# Demonstração do uso de operadores!
+X = int(input("Digite um número:"))
+Y = int(input("Digite outro número:"))
+
+SOMA = X + Y
+print("O resultado da operação é:", SOMA)
+
+SUBTRAÇÃO = X - Y 
+print("O resultado da subtração é:", SUBTRAÇÃO)
+
+MULTIPLICAÇÃO = X * Y 
+print("O resultado da multiplicação é:", MULTIPLICAÇÃO)
+
+DIVISAO = X / Y
+print("O resultado da divisão é:", DIVISAO)
