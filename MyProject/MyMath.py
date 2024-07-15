50803ba7f63e5e8a61648313df2effe9c1536962
Kamilly Alessandra Oliveira da Silva <kamilli.alessandra11@gmail.com>
2024-07-15 08:36:33 -0300

    Calculos matematicos soma e media...

---
+++ MyProject/MyMath.py
@@ -0,0 +1,11 @@
+# Demonstração do uso de operadores
+# Criar um programa que faça o somatório de 3 valores e tire sua média...
+
+X = int (input("Digite o valor 1:"))
+Y = int (input("Digite o valor 2:"))
+Z = int (input("Digite o valor 3:"))
+soma = X + Y + Z
+media = soma / 3
+
+print("A soma é:", soma)
+print(f"A média é:, {media}")
