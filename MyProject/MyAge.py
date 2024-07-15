f6454d8c6d8b3fc477fdb7f62927c6d7c0879781
Kamilly Alessandra Oliveira da Silva <kamilli.alessandra11@gmail.com>
2024-07-15 10:34:57 -0300

    Demonstrando as estruturas condicionais...

---
+++ MyProject/MyAge.py
@@ -0,0 +1,9 @@
+# Demonstração do uso de if/elif/else...
+print("Digite a sua idade:")
+idade = int(input())
+
+if idade < 18:
+    print("Você não é maior de idade!")
+    print("Não poderá realizar a operação!")
+
+print("Obrigado por optar pelos nossos serviços!")
