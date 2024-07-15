c20f5c0046894b82edca9c917a9d6c027015de34
Kamilly Alessandra Oliveira da Silva <kamilli.alessandra11@gmail.com>
2024-07-15 09:26:56 -0300

    Desenvolvendo atividades que o professor passou

---
+++ MyProject/MyCalcs -1.py
@@ -0,0 +1,8 @@
+# Demonstração...Atividade 1
+# Construa um programa onde o usuário digitará dois números e o programa dará, como resultado, a soma de dois números.
+
+x = int (input("Digite um número:"))
+y = int (input("Digite um número:"))
+
+SOMA = x + y
+print("O resultado da operação é:", SOMA)
