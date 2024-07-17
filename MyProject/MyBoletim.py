2de69572b66182e4500e8b77f7b1f7f1a2d7b86e
Kamilly Alessandra Oliveira da Silva <kamilli.alessandra11@gmail.com>
2024-07-17 11:52:27 -0300

    Desenvolvendo...

---
+++ MyProject/MyBoletim.py
@@ -0,0 +1,25 @@
+# Construa uma página onde o usuário digitará duas notas escolares.
+# Casoa média seja abaixo de 4, o programa escreverá: "Aluno reprovado".
+# Caso a nota seja maior que 4 e menor que 6 escreverá: "Aluno em recuperação". 
+# No úlimo caso, o programa deve solicitar anota de recuperação. 
+# Caso ela seja menor que 5, escrever: "Reprovado na recuperação" ou, caso contrario, escrever "Aprovação na recuperação".
+
+nota1 = float(input("Digite a prijmeira nota : "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+if media < 4:
+    print("Aluno Reprovado")
+elif media >= 4 and media < 6:
+    print("Aluno em recuperação")
+
+notaderecuperação = float(input("Digite a nota da recuperação:"))
+
+if media < 5:
+    print("Reprovado na recuperação")
+else:
+    print("Aprovado na recuperação")
+elif:
+media >= 6:
+    print("Aluno Aprovado")
