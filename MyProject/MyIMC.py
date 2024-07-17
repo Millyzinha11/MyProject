d3d076cf6b19141f37c29c771150918b58343e42
Kamilly Alessandra Oliveira da Silva <kamilli.alessandra11@gmail.com>
2024-07-17 11:24:17 -0300

    Desenvolvendo...

--- MyProject/MyIMC.py
+++ MyProject/MyIMC.py
@@ -3,11 +3,12 @@
 # Caso o IMC seja maior que 25 exibirá, na tela, "Acima do peso ideal"
 # Caso seja menor que 18, "Abaixo do peso ideal". Por fim, se o resultado do cálculo entregar um valor entre 18 e 25, informar que " O seu peso eestá OK".
 
-peso = float(input("Digite o seu peso:"))
+peso = int(input("Digite o seu peso:"))
 altura = float(input("Digite sua altura:"))
 
 imc = peso / altura ** 2
 
+print(f"O seu imc é {imc}.")
 if imc > 25 :
     print("Acima do peso ideal")
 elif imc < 18:
