4d53d80a2dd409a5f640e71c3f8b3c2267c5da25
Kamilly Alessandra Oliveira da Silva <kamilli.alessandra11@gmail.com>
2024-07-19 11:51:48 -0300

    Desenvolvendo...

---
+++ MyProject/MyRetirement.py
@@ -0,0 +1,5 @@
+# Construa um programa que verifique se um trabalhador está qualificado para se aposentar (ou não). 
+# Para isto, este programa deverá verificar o sexo do empregado (masculino ou feminino) e com base nele, analisar os demais critérios, 
+# como a idade, o tempo de contribuição e (opcional) a exposição a insalubridade. Com base nestas variáveis ( e outras, ficando á critério do desenvovedor), 
+# o programa deverá realizar os cálculos necessários para descobrir a sua pontuação e assim, notificar ao trabalhador em questão se ele está apto para se aposentar (ou não)...
+
