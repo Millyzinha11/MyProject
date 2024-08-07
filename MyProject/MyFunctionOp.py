ce05effbc3a0f1350f6dddc5d841e087666d1c36
Kamilly Alessandra Oliveira da Silva <kamilli.alessandra11@gmail.com>
2024-08-07 10:41:50 -0300

    Desenvolvimento...

--- MyProject/MyFunctionOp.py
+++ MyProject/MyFunctionOp.py
@@ -1,4 +1,8 @@
 # Demonstração do uso de funções...
+def adicao (x,y):
+    return x + y
+def subtracao (x,y):
+    return x - y
 def multiplicacao (x,y):
     return x * y
 def divisao (x,y):
@@ -7,11 +11,17 @@ def divisao (x,y):
 print("Digite dois valores inteiros...")
 n1 = int(input("x: "))
 n2 = int(input("y: "))
-op = input("qual operação (* ou /)?")
+op = input("qual operação (+,-,* ou /)?")
 
-if op == "*":
-    z = multiplicacao(n1,n2)
-    print("resultado da multiplicacao:", z)
+if op == "+":
+    z = adicao(n1,n2)
+    print("resultado da soma:", z)
+elif op == "-":
+    z = subtracao (n1,n2)
+    print("resultado da subtracao:", z )
+elif op == "*":
+    z = multiplicacao (n1,n2)
+    print("resultado da multiplicacao:", z )
 elif op == "/":
     z = divisao (n1,n2)
     print("resultado da divisao:", z )
