051e6e218a4d0781396f7f71d0c439c1848d22fd
Kamilly Alessandra Oliveira da Silva <kamilli.alessandra11@gmail.com>
2024-08-12 11:50:40 -0300

    Atividade 12/08

---
+++ MyProject/MyGabarit.py
@@ -0,0 +1,8 @@
+# Demonstração de funções em listas
+print("Verifique suas respostas de acordo com o gabarito:")
+gabarito = ["B", "C", "A", "E", "D​"]
+
+resposta = []
+for x in range(5):
+        print(f'Digite a resposta da questão {x+1},(letra maiúscula):')
+        resposta.append(resposta)
